f2ce00e2265f5d253a217311354c80d8f060969a
vinay-abd <karchi.vinay@gmail.com>
2020-03-04 12:23:56 +0530

    Add files via upload
    JWT all required resource files.

---
+++ JWT/Resources/AuthenticationException.java
@@ -0,0 +1,7 @@
+package in.net.usit.JWT.Resources;
+public class AuthenticationException extends RuntimeException {
+    public AuthenticationException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
+
